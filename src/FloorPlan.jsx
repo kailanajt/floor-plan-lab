3be6848495d0ec372ac256754a495162153ac545
kailanajt <kailana.thompson12@gmail.com>
2022-07-06 16:00:50 -0500

    complete lab now

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -6,23 +6,17 @@ import Bath from "./Bath"
 function FloorPlan (props) {
   return (
     <>
-        <div className=''>
-          <LivingRoom />
-          <Bath size={'Half'} />
-        </div>
-        <div className="first">
-          <Bedroom bedNum={1} />
-          <Bath size={'Full'} />
-        </div>
-        <div className="second">
-          <Bedroom bedNum={2} />
-        </div>
-        <div className="kitchen">
-          <Kitchen />
-        </div>
-        <div className="third">
-          <Bedroom bedNum={3} />
+      <div className ='LivingRoom'>
+      <LivingRoom />
+      <Bath size={'Half'} />
       </div>
+      <div className = 'MasterBed'>
+      <Bedroom bedNum={1} />
+      <Bath size={'Full'} />
+      </div>
+      <Bedroom bedNum={2} />
+      <Kitchen />
+      <Bedroom bedNum={3} />
     </>
   )
 }
