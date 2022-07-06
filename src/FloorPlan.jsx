b100a1f4e893e23fc75d52501ae12379c6c5d0fa
kailanajt <kailana.thompson12@gmail.com>
2022-07-06 15:53:34 -0500

    small adjustments made to floorplan

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -10,15 +10,15 @@ function FloorPlan (props) {
           <LivingRoom />
           <Bath size={'Half'} />
         </div>
-        <div>
+        <div className="first">
           <Bedroom bedNum={1} />
           <Bath size={'Full'} />
         </div>
-        <div className="first">
-          <Kitchen />
-        </div>
         <div className="second">
           <Bedroom bedNum={2} />
+        </div>
+        <div className="kitchen">
+          <Kitchen />
         </div>
         <div className="third">
           <Bedroom bedNum={3} />
